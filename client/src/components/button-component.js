fc335bac6e680efe1e794e292be48a426adc2530
Alden Vallestero <alden.v@agtuary.com>
2024-01-10 13:59:24 +0800

    🐛 Fix Callback Function Name

--- client/src/components/button-component.js
+++ client/src/components/button-component.js
@@ -1,7 +1,7 @@
-export const Button =({ placeholder, cb, label }) => {
+export const Button =({ placeholder, callback, label }) => {
   return (
     <div>
-      <button className="bg-blue-600 text-white font-bold rounded-md py-1 w-1/3" onClick={cb}>{placeholder}</button>
+      <button className="bg-blue-600 text-white font-bold rounded-md py-1 w-1/3" onClick={callback}>{placeholder}</button>
       { label && <small className="block text-gray-500">{label}</small> }
     </div>
   )
